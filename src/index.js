998bde6c6e3a6e7c9ea19cbea69b63aea436a70a
altynai91 <lenta.kg@gmail.com>
2022-01-06 23:17:49 +0300

    docs: update indexjs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,49 @@
 module.exports = function toReadable (number) {
-  
+  let newNum = String(number).split('');
+  let answer = [];
+  let simple = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  let decimals = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  let hundred = ['hundred'];
+  if(number >= 0 && number < 20) {
+    for(let i = 0; i < 1; i++) {
+      answer.push(simple[number]);
+      return answer.join('');
+    }
+  }
+  else if(number >= 20 && number < 100) {
+    for(let j = 0; j < 1; j++) {
+      if (newNum[1] === '0') {
+        answer.push(decimals[newNum[0] - 2]);
+        return answer.join('');
+      }
+      else {
+        answer.push(decimals[newNum[0] - 2] + ' ' + simple[newNum[1]]);
+        return answer.join('');
+      }
+    }
+  }
+  else if(number >= 100 && number < 1000) {
+    for(let k = 0; k < 1; k++) {
+      if (newNum[1] === '0' && newNum[2] > 0) {
+        answer.push(simple[newNum[0]] + ' ' + hundred + ' ' + simple[newNum[2]]);
+        return answer.join('');
+      }
+      else if (newNum[1] === '1') {
+        answer.push(simple[newNum[0]] + ' ' + hundred + ' ' + simple[newNum[1] + newNum[2]]);
+        return answer.join('');
+      }
+      else if (newNum[1] > 1 && newNum[2] != '0') {
+        answer.push(simple[newNum[0]] + ' ' + hundred + ' ' + decimals[newNum[1] - 2] + ' ' + simple[newNum[2]]);
+        return answer.join('');
+      }
+      else if (newNum[1] > 1 && newNum[1] != '0' && newNum[2] === '0') {
+        answer.push(simple[newNum[0]] + ' ' + hundred + ' ' + decimals[newNum[1] - 2]);
+        return answer.join('');
+      }
+      else if (newNum[1] === '0' && newNum[2] === '0') {
+        answer.push(simple[newNum[0]] + ' ' + hundred);
+        return answer.join('');
+      }
+    }
+  }
 }
